40f6dc791362b1828b15e6abac67a50bd7685b9c
Stradios <Theodor.veres.85@gmail.com>
2023-08-07 08:20:40 +0200

    0.1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Minecraft-fonts
+# Segoe-UI Font on Linux (Ubuntu)
+
+
+
+
+
+
+<p align="center"><img src="https://img.fontspace.co/gallery/750x500/5/869d88a2ff5d4b2f98213ac1cd6f9796.png" alt="segoe" width="600"/></div></p>
+
+<p align="center"><img src="https://img.fontspace.co/preview/d/824ab5acb86547fd8834fe4b251c508d/minecraft-evenings-font-screenshot-dark-32422-600x300.png" alt="segoe" width="600"/></div></p> 
+
+
+## How to install it
+
+Open `terminal` and run the command below
+
+```
+git clone https://github.com/Stradios/Minecraft-fonts.git
+chmod +x install.sh
+./install.sh
+```
+
+
+
+
---
+++ install.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+# Colours Variables
+RESTORE='\033[0m'
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+PURPLE='\033[00;35m'
+CYAN='\033[00;36m'
+LIGHTGRAY='\033[00;37m'
+LRED='\033[01;31m'
+LGREEN='\033[01;32m'
+LYELLOW='\033[01;33m'
+LBLUE='\033[01;34m'
+LPURPLE='\033[01;35m'
+LCYAN='\033[01;36m'
+WHITE='\033[01;37m'
+
+# Destination directory
+#ROOT_UID=0
+DEST_DIR="/usr/share/fonts/MinecraftTen-VGORe/TrueType/Minecraft/"
+WINE_FONT_DIR="$HOME/.wine/drive_c/windows/Fonts/"
+
+#if [ "$UID" -eq "$ROOT_UID" ]; then
+#  DEST_DIR="/usr/local/share/fonts/MinecraftTen-VGORe/TrueType/Minecraft/
+#else
+#  DEST_DIR="$HOME/.local/share/fonts/MinecraftTen-VGORe/TrueType/Minecraft/"
+#fi
+
+# Check Internet Conection
+function cekkoneksi(){
+    echo -e "$BLUE [ * ] Checking for internet connection"
+    sleep 1
+    echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "$RED [ X ]$BLUE Internet Connection ➜$RED OFFLINE!\n";
+        echo -e "$RED Sorry, you really need an internet connection...."
+        exit 0
+    else
+        echo -e "$GREEN [ ✔ ]$BLUE Internet Connection ➜$GREEN CONNECTED!\n";
+        sleep 1
+    fi
+}
+
+function cekwget(){
+    echo -e "$BLUE [ * ] Checking for Wget"
+    sleep 1
+    which wget > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+    echo -e "$GREEN [ ✔ ]$BLUE Wget ➜$GREEN INSTALLED\n"
+        sleep 1
+    else
+        echo -e "$RED [ X ]$BLUE Wget ➜$RED NOT INSTALLED\n"
+        continueWget
+    fi
+}
+
+function cekfont(){
+    echo -e "$BLUE [ * ] Checking for Minecraft Font"
+    sleep 1
+    fc-list | grep -i "Minecraft" >/dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+    echo -e "$GREEN [ ✔ ]$BLUE Minecraft Font ➜$GREEN INSTALLED\n"
+        sleep 1
+    else
+        echo -e "$RED [ X ]$BLUE Minecraft Font ➜$RED NOT INSTALLED\n"
+        continueFont
+    fi
+}
+
+function continueFont(){
+    echo -e "$LGREEN Do you want to install Minecraft Font? (y)es, (n)o :"
+    read  -p ' ' INPUT
+    case $INPUT in
+    [Yy]* ) fontinstall;;
+    [Nn]* ) end;;
+    * ) echo -e "$RED\n Sorry, try again."; continueFont;;
+  esac
+}
+
+function fontinstall(){
+    sudo mkdir -p "$DEST_DIR"
+    if [ -d font ]; then
+        cp font/MinecraftTen.ttf "$DEST_DIR"/MinecraftTen.ttf > /dev/null 2>&1 # MinecraftTen        
+        cp font/MinecraftEvenings.ttf "$DEST_DIR"/MinecraftEvenings.ttf > /dev/null 2>&1 # MinecraftEvenings
+        if [ -d $WINE_FONT_DIR ]; then
+            cp font/MinecraftTen.ttf "$WINE_FONT_DIR"/MinecraftTen.ttf > /dev/null 2>&1 # MinecraftTen
+            cp font/MinecraftEvenings.ttf "$WINE_FONT_DIR"/MinecraftEvenings.ttf > /dev/null 2>&1 # MinecraftEvenings
+            echo -e "$GREEN\n Font installed to WINE $LBLUE'$WINE_FONT_DIR'"
+        fi
+
+    else
+        # Download font from github static link code
+        wget -q https://github.com/mrbvrz/segoe-ui/raw/master/font/segoeui.ttf?raw=true -O "$DEST_DIR"/segoeui.ttf > /dev/null 2>&1 # regular
+        wget -q https://github.com/mrbvrz/segoe-ui/raw/master/font/segoeui.ttf?raw=true -O "$DEST_DIR"/segoeui.ttf > /dev/null 2>&1 # regular
+
+    fi
+
+    fc-cache -f "$DEST_DIR"
+    echo -e "$GREEN\n Font installed on $LBLUE'$DEST_DIR'"
+}
+
+function wgetinstall(){   
+    sleep 1
+    sudo apt update > /dev/null 2>&1
+    sudo apt install -y wget > /dev/null 2>&1
+}
+
+function end(){
+    echo -e "$LPURPLE\n Bye..... ;)"
+    exit 0
+}
+
+continueWget() {
+  echo -e "$LGREEN Do you want to install Wget? (y)es, (n)o :"
+  read  -p ' ' INPUT
+  case $INPUT in
+    [Yy]* ) wgetinstall;;
+    [Nn]* ) end;;
+    * ) echo -e "$RED\n Sorry, try again."; continueWget;;
+  esac
+}
+
+function banner(){
+    echo -e "$LYELLOW" ""
+    echo -e "MinecraftTen-VGORe Font "
+
+}
+
+main(){
+    clear
+    banner
+    cekkoneksi
+    cekwget
+    cekfont
+}
+
+main
